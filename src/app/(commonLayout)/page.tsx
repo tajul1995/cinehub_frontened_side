937ee3a1fb2a82c5b3f53b548755c35ceb379e7d
tajul islam <tajulislam199595@gmail.com>
2026-03-27 20:03:00 +0600

    gallary-commit

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,10 +1,12 @@
 import BannerCarousel from "@/components/home/Banner";
+import PopularGallery from "@/components/home/PopularGallery";
 
 
 export default function Home() {
   return (
-    <div className="w-7xl mx-auto">
+    <div className="max-w-7xl overflow-x-hidden mx-auto">
      <BannerCarousel></BannerCarousel>
+     <PopularGallery></PopularGallery>
     </div>
   );
 }
